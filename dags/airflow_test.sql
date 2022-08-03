ca049f74749447c2467d06f071d61e4132a8a08f
edifierc3 <ehsuanwu@users.noreply.github.com>
2022-08-02 18:21:54 -0700

    add load_utc_ts to airflow_function_test

--- dags/airflow_test.sql
+++ dags/airflow_test.sql
@@ -1,3 +1,3 @@
-CREATE OR REPLACE TRANSIENT TABLE airflow_function_test (name VARCHAR(250), id INT);
+CREATE OR REPLACE TRANSIENT TABLE airflow_function_test (name VARCHAR(250), id INT, load_utc_ts datetime);
 
-INSERT INTO airflow_function_test VALUES ('name', 5);
+INSERT INTO airflow_function_test VALUES ('name', 5, sysdate());
