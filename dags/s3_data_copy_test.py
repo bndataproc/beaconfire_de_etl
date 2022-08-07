cb9cb85d28e68a4139b47bfb40e09c8ad6f88ea1
bndataproc <ewu@beaconfireinc.com>
2022-08-07 15:06:20 -0700

    test stage

--- dags/s3_data_copy_test.py
+++ dags/s3_data_copy_test.py
@@ -22,7 +22,7 @@ with DAG(
     "s3_data_copy_test",
     start_date=datetime(2022, 7, 12),
     end_date = datetime(2022, 7, 14),
-    schedule_interval='* * * * *',
+    schedule_interval='0 7 * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire'],
     catchup=True,
