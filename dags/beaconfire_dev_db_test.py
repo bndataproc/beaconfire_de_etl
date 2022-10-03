4aa3c8d65f61d79641e09a4f6e4937edb61cb800
unknown <zhongyiting98@gmail.com>
2022-10-03 16:59:18 -0400

    group_2

--- dags/beaconfire_dev_db_test.py
+++ dags/beaconfire_dev_db_test.py
@@ -18,7 +18,7 @@ SNOWFLAKE_WAREHOUSE = 'aw_etl'
 SNOWFLAKE_STAGE = 'beaconfire_stage'
  #S3_FILE_PATH = '</path/to/file/sample_file.csv'
 
-SNOWFLAKE_SAMPLE_TABLE = 'airflow_testing'
+SNOWFLAKE_SAMPLE_TABLE = 'airflow_testing_group2'
 
 # SQL commands
 CREATE_TABLE_SQL_STRING = (
