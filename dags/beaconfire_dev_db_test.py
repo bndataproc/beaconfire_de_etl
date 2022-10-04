fce9d7c69c82a9b3a6917029ece36617cf41d5c0
unknown <zhongyiting98@gmail.com>
2022-10-04 14:41:53 -0400

    test

--- dags/beaconfire_dev_db_test.py
+++ dags/beaconfire_dev_db_test.py
@@ -6,8 +6,9 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-#from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
-#branch_group2_test1
+
+# from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+# branch_group2_test1
 
 SNOWFLAKE_CONN_ID = 'snowflake_conn'
 SNOWFLAKE_DATABASE = 'beaconfire'
@@ -16,28 +17,28 @@ SNOWFLAKE_SCHEMA = 'dev_db'
 SNOWFLAKE_ROLE = 'AW_developer'
 SNOWFLAKE_WAREHOUSE = 'aw_etl'
 SNOWFLAKE_STAGE = 'beaconfire_stage'
- #S3_FILE_PATH = '</path/to/file/sample_file.csv'
+# S3_FILE_PATH = '</path/to/file/sample_file.csv'
 
-SNOWFLAKE_SAMPLE_TABLE = 'airflow_testing_group2'
+SNOWFLAKE_SAMPLE_TABLE = 'airflow_group2_SYMBOLS'
 
 # SQL commands
 CREATE_TABLE_SQL_STRING = (
-    f"CREATE OR REPLACE TRANSIENT TABLE {SNOWFLAKE_SAMPLE_TABLE} (name VARCHAR(250), id INT);"
+    f"CREATE OR REPLACE TRANSIENT TABLE {SNOWFLAKE_SAMPLE_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.SYMBOLS;"
 )
-SQL_INSERT_STATEMENT = f"INSERT INTO {SNOWFLAKE_SAMPLE_TABLE} VALUES ('name', %(id)s)"
-SQL_LIST = [SQL_INSERT_STATEMENT % {"id": n} for n in range(0, 10)]
-SQL_MULTIPLE_STMTS = "; ".join(SQL_LIST)
-#ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+# SQL_INSERT_STATEMENT = f"INSERT INTO {SNOWFLAKE_SAMPLE_TABLE} select * from US_STOCKS_DAILY.PUBLIC.SYMBOLS"
+# SQL_LIST = [SQL_INSERT_STATEMENT % {"id": n} for n in range(0, 10)]
+# SQL_MULTIPLE_STMTS = "; ".join(SQL_LIST)
+# ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "beaconfire_dev_db_test"
 # [START howto_operator_snowflake]
 
 with DAG(
-    DAG_ID,
-    start_date=datetime(2021, 1, 1),
-    schedule_interval='07 * * * *',
-    default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
-    tags=['beaconfire'],
-    catchup=False,
+        DAG_ID,
+        start_date=datetime(2021, 1, 1),
+        schedule_interval='07 * * * *',
+        default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+        tags=['beaconfire'],
+        catchup=False,
 ) as dag:
     # [START snowflake_example_dag]
     snowflake_op_sql_str = SnowflakeOperator(
@@ -49,27 +50,27 @@ with DAG(
         role=SNOWFLAKE_ROLE,
     )
 
-    snowflake_op_with_params = SnowflakeOperator(
-        task_id='snowflake_op_with_params',
-        sql=SQL_INSERT_STATEMENT,
-        parameters={"id": 11},
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        database=SNOWFLAKE_DATABASE,
-        schema=SNOWFLAKE_SCHEMA,
-        role=SNOWFLAKE_ROLE,
-    )
+    # snowflake_op_with_params = SnowflakeOperator(
+    #     task_id='snowflake_op_with_params',
+    #     sql=SQL_INSERT_STATEMENT,
+    #     parameters={"id": 11},
+    #     warehouse=SNOWFLAKE_WAREHOUSE,
+    #     database=SNOWFLAKE_DATABASE,
+    #     schema=SNOWFLAKE_SCHEMA,
+    #     role=SNOWFLAKE_ROLE,
+    # )
+    #
+    # snowflake_op_sql_list = SnowflakeOperator(task_id='snowflake_op_sql_list', sql=SQL_LIST)
+    #
+    # snowflake_op_sql_multiple_stmts = SnowflakeOperator(
+    #     task_id='snowflake_op_sql_multiple_stmts',
+    #     sql=SQL_MULTIPLE_STMTS,
+    # )
 
-    snowflake_op_sql_list = SnowflakeOperator(task_id='snowflake_op_sql_list', sql=SQL_LIST)
-
-    snowflake_op_sql_multiple_stmts = SnowflakeOperator(
-        task_id='snowflake_op_sql_multiple_stmts',
-        sql=SQL_MULTIPLE_STMTS,
-    )
-
-    snowflake_op_template_file = SnowflakeOperator(
-       task_id='snowflake_op_template_file',
-       sql='./beaconfire_dev_db_test.sql',
-    )
+    # snowflake_op_template_file = SnowflakeOperator(
+    #    task_id='snowflake_op_template_file',
+    #    sql='./beaconfire_dev_db_test.sql',
+    # )
 
     # [END howto_operator_snowflake]
 
@@ -86,17 +87,16 @@ with DAG(
 
     # [END howto_operator_s3_to_snowflake]
 
-
     (
         snowflake_op_sql_str
-        >> [
-            snowflake_op_with_params,
-            snowflake_op_sql_list,
-            snowflake_op_template_file,
-            # copy_into_table,
-            snowflake_op_sql_multiple_stmts,
-        ]
-        
+        # >> [
+        #     snowflake_op_with_params,
+        #     snowflake_op_sql_list,
+        #     snowflake_op_template_file,
+        #     # copy_into_table,
+        #     snowflake_op_sql_multiple_stmts,
+        # ]
+
     )
     # [END snowflake_example_dag]
 
