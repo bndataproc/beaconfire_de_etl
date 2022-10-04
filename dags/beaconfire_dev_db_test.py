47f34c6dac46e54cb00361bfef1740e38b5fbb3f
JunzhiGe <junzhige98@gmail.com>
2022-10-04 19:26:37 -0400

    add something

--- dags/beaconfire_dev_db_test.py
+++ dags/beaconfire_dev_db_test.py
@@ -6,9 +6,8 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-
-# from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
-# branch_group2_test1
+#from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+#branch_group2_test1
 
 SNOWFLAKE_CONN_ID = 'snowflake_conn'
 SNOWFLAKE_DATABASE = 'beaconfire'
@@ -17,33 +16,66 @@ SNOWFLAKE_SCHEMA = 'dev_db'
 SNOWFLAKE_ROLE = 'AW_developer'
 SNOWFLAKE_WAREHOUSE = 'aw_etl'
 SNOWFLAKE_STAGE = 'beaconfire_stage'
-# S3_FILE_PATH = '</path/to/file/sample_file.csv'
+ #S3_FILE_PATH = '</path/to/file/sample_file.csv'
 
 SNOWFLAKE_SAMPLE_TABLE = 'airflow_group2_SYMBOLS'
+FACT_TABLE = 'GROUP2_STOCK'
+DIM_TABLE = 'GROUP2_DIM_COMPANY'
+
 
 # SQL commands
-CREATE_TABLE_SQL_STRING = (
-    f"CREATE OR REPLACE TRANSIENT TABLE {SNOWFLAKE_SAMPLE_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.SYMBOLS;"
-)
+#CREATE_FACT_TABLE_SQL_STRING = (
+#    f"CREATE OR REPLACE TRANSIENT TABLE {FACT_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY;"
+#)
+
+#CREATE_DIM_TABLE_SQL_STRING = (
+#    [f"CREATE OR REPLACE TRANSIENT TABLE {DIM_TABLE} as select  from US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY;",
+#     f"ALTER TABLE {DIM_TABLE} MODIFY ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY"]
+#)
+
+UPDATE_DIM_TABLE = f"CREATE OR REPLACE TRANSIENT TABLE {DIM_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.COMPANY_PROFILE;"
+
+
+UPDATE_FACT_TABLE = f"INSERT INTO {FACT_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY WHERE DATE = getdate();"
+
+
 # SQL_INSERT_STATEMENT = f"INSERT INTO {SNOWFLAKE_SAMPLE_TABLE} select * from US_STOCKS_DAILY.PUBLIC.SYMBOLS"
 # SQL_LIST = [SQL_INSERT_STATEMENT % {"id": n} for n in range(0, 10)]
 # SQL_MULTIPLE_STMTS = "; ".join(SQL_LIST)
-# ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+#ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "beaconfire_dev_db_test"
 # [START howto_operator_snowflake]
 
 with DAG(
-        DAG_ID,
-        start_date=datetime(2021, 1, 1),
-        schedule_interval='07 * * * *',
-        default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
-        tags=['beaconfire'],
-        catchup=False,
+    DAG_ID,
+    start_date=datetime(2021, 1, 1),
+    schedule_interval='07 * * * *',
+    default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+    tags=['beaconfire'],
+    catchup=False,
 ) as dag:
     # [START snowflake_example_dag]
-    snowflake_op_sql_str = SnowflakeOperator(
-        task_id='snowflake_op_sql_str',
-        sql=CREATE_TABLE_SQL_STRING,
+    #snowflake_op_sql_str = SnowflakeOperator(
+    #    task_id='snowflake_op_sql_str',
+    #    sql=CREATE_TABLE_SQL_STRING,
+    #    warehouse=SNOWFLAKE_WAREHOUSE,
+    #    database=SNOWFLAKE_DATABASE,
+    #    schema=SNOWFLAKE_SCHEMA,
+    #    role=SNOWFLAKE_ROLE,
+    #)
+
+    snowflake_dim_update = SnowflakeOperator(
+        task_id='snowflake_dim_update',
+        sql=UPDATE_DIM_TABLE,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA,
+        role=SNOWFLAKE_ROLE,
+    )
+
+    snowflake_fact_update = SnowflakeOperator(
+        task_id='snowflake_fact_update',
+        sql=UPDATE_FACT_TABLE,
         warehouse=SNOWFLAKE_WAREHOUSE,
         database=SNOWFLAKE_DATABASE,
         schema=SNOWFLAKE_SCHEMA,
@@ -87,8 +119,11 @@ with DAG(
 
     # [END howto_operator_s3_to_snowflake]
 
+
     (
-        snowflake_op_sql_str
+        snowflake_dim_update >> snowflake_fact_update
+
+        #snowflake_op_sql_str
         # >> [
         #     snowflake_op_with_params,
         #     snowflake_op_sql_list,
--- dags/group2_stocks.py
+++ dags/group2_stocks.py
@@ -19,11 +19,26 @@ SNOWFLAKE_STAGE = 'beaconfire_stage'
  #S3_FILE_PATH = '</path/to/file/sample_file.csv'
 
 SNOWFLAKE_SAMPLE_TABLE = 'airflow_group2_SYMBOLS'
+FACT_TABLE = 'GROUP2_STOCK'
+DIM_TABLE = 'GROUP2_DIM_COMPANY'
+
 
 # SQL commands
-CREATE_TABLE_SQL_STRING = (
-    f"CREATE OR REPLACE TRANSIENT TABLE {SNOWFLAKE_SAMPLE_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.SYMBOLS;"
-)
+#CREATE_FACT_TABLE_SQL_STRING = (
+#    f"CREATE OR REPLACE TRANSIENT TABLE {FACT_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY;"
+#)
+
+#CREATE_DIM_TABLE_SQL_STRING = (
+#    [f"CREATE OR REPLACE TRANSIENT TABLE {DIM_TABLE} as select  from US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY;",
+#     f"ALTER TABLE {DIM_TABLE} MODIFY ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY"]
+#)
+
+UPDATE_DIM_TABLE =
+f"CREATE OR REPLACE TRANSIENT TABLE {DIM_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.COMPANY_PROFILE;"
+
+UPDATE_FACT_TABLE =
+f"INSERT INTO {FACT_TABLE} as select * from US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY WHERE DATE = getDate();"
+
 # SQL_INSERT_STATEMENT = f"INSERT INTO {SNOWFLAKE_SAMPLE_TABLE} select * from US_STOCKS_DAILY.PUBLIC.SYMBOLS"
 # SQL_LIST = [SQL_INSERT_STATEMENT % {"id": n} for n in range(0, 10)]
 # SQL_MULTIPLE_STMTS = "; ".join(SQL_LIST)
@@ -40,9 +55,27 @@ with DAG(
     catchup=False,
 ) as dag:
     # [START snowflake_example_dag]
-    snowflake_op_sql_str = SnowflakeOperator(
-        task_id='snowflake_op_sql_str',
-        sql=CREATE_TABLE_SQL_STRING,
+    #snowflake_op_sql_str = SnowflakeOperator(
+    #    task_id='snowflake_op_sql_str',
+    #    sql=CREATE_TABLE_SQL_STRING,
+    #    warehouse=SNOWFLAKE_WAREHOUSE,
+    #    database=SNOWFLAKE_DATABASE,
+    #    schema=SNOWFLAKE_SCHEMA,
+    #    role=SNOWFLAKE_ROLE,
+    #)
+
+    snowflake_dim_update = SnowflakeOperator(
+        task_id='snowflake_dim_update',
+        sql=UPDATE_DIM_TABLE,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA,
+        role=SNOWFLAKE_ROLE,
+    )
+
+    snowflake_fact_update = SnowflakeOperator(
+        task_id='snowflake_fact_update',
+        sql=UPDATE_FACT_TABLE,
         warehouse=SNOWFLAKE_WAREHOUSE,
         database=SNOWFLAKE_DATABASE,
         schema=SNOWFLAKE_SCHEMA,
@@ -88,7 +121,9 @@ with DAG(
 
 
     (
-        snowflake_op_sql_str
+        snowflake_dim_update >> snowflake_fact_update
+
+        #snowflake_op_sql_str
         # >> [
         #     snowflake_op_with_params,
         #     snowflake_op_sql_list,
