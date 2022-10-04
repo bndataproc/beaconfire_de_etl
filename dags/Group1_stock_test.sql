f135f71cc3d93725a7dbd49e3e33763d5ddc103c
Yanlin Xiang <yanlinxiang01@gmail.com>
2022-10-04 14:51:56 -0400

    change sql

--- dags/Group1_stock_test.sql
+++ dags/Group1_stock_test.sql
@@ -1,7 +1,3 @@
-CREATE database stock_group1_dev;
-create schema group1_schema;
-
-
-CREATE TABLE stock_group1_dev.group1_schema.SYMBOLS_group1 as SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."SYMBOLS";
-CREATE TABLE stock_group1_dev.group1_schema.COMPANY_PROFILE_group1 as SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."COMPANY_PROFILE";
-CREATE TABLE stock_group1_dev.group1_schema.stock_history_group1 as SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."stock_history";
+CREATE TABLE BEACONFIRE.DEV_DB.SYMBOLS_group1 as SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."SYMBOLS";
+CREATE TABLE BEACONFIRE.DEV_DB.COMPANY_PROFILE_group1 as SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."COMPANY_PROFILE";
+CREATE TABLE BEACONFIRE.DEV_DB.stock_history_group1 as SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."stock_history";
