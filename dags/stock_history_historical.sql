1ef1864135757e98ea76f9c81cd5b0bace446d40
Zehui Yuan <zehuiyuan@BeaconFires-MacBook-Pro.local>
2022-10-20 17:16:27 -0400

    s3

--- dags/stock_history_historical.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO stock_history_historical
-    SELECT
-        *
-        ,current_date() as load_id
-        ,sha2(symbol) as symbol_id
-    FROM "US_STOCKS_DAILY"."PUBLIC"."STOCK_HISTORY"
