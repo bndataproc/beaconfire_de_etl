43eaa8f2a745eaee826221164c37e9d99f0efc22
Zehui Yuan <zehuiyuan@BeaconFires-MacBook-Pro.local>
2022-10-21 13:23:59 -0400

    s3

--- dags/customer_payment_s3_incre.py
+++ dags/customer_payment_s3_incre.py
@@ -17,6 +17,19 @@ SNOWFLAKE_WAREHOUSE = 'aw_etl'
 SNOWFLAKE_STAGE = 's3_stage_customer_payment'
 # S3_FILE_PATH = 'customer_payment_10182022.csv'
 
+SNOWFLAKE_SAMPLE_TABLE = 'customer_payment_prestg'
+CREATE_TABLE_SQL_STRING = (
+    f'''CREATE OR REPLACE TEMP TABLE {SNOWFLAKE_SAMPLE_TABLE} (
+        InvoiceNo varchar
+        ,StockCode varchar
+        ,Description varchar
+        ,Quantity int
+        ,InvoiceDate timestamp
+        ,UnitPrice number(10,2)
+        ,CustomerID int
+        ,Country varchar);
+    '''
+)
 with DAG(
         "group1_s3_copy_test",
         start_date=datetime(2022, 10, 18),
@@ -26,10 +39,19 @@ with DAG(
         tags=['test'],
         catchup=True,
 ) as dag:
+    create_prestg_table = SnowflakeOperator(
+        task_id='snowflake_create_prestg_table',
+        sql=CREATE_TABLE_SQL_STRING,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA,
+        role=SNOWFLAKE_ROLE,
+    )
+
     copy_into_prestg = S3ToSnowflakeOperator(
         task_id='prestg_customer_payment',
         s3_keys=['customer_payment_{{ ds[5:7]+ds[8:10]+ds[0:4] }}.csv'],
-        table='prestg_customer_payment',
+        table='customer_payment_prestg',
         schema=SNOWFLAKE_SCHEMA,
         stage=SNOWFLAKE_STAGE,
         file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \
@@ -37,7 +59,7 @@ with DAG(
             ESCAPE_UNENCLOSED_FIELD = NONE RECORD_DELIMITER = '\n')''',
     )
 
-    copy_into_prestg
+    create_prestg_table >> copy_into_prestg
 
 
 
